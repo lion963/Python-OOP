dcf51416b929430c4c080d79a0a93cbe808e6c15
Asen Kamboshev <lion963@mail.bg>
2021-02-23 10:11:54 +0200

    defining classes 22.02

---
+++ classes_and_instances_exercise/time.py
@@ -0,0 +1,47 @@
+class Time():
+    max_hours = 23
+    max_minutes = 59
+    max_seconds = 59
+
+    def __init__(self, hours, minutes, seconds):
+        self.hours = hours
+        self.minutes = minutes
+        self.seconds = seconds
+
+    def set_time(self, hours, minutes, seconds):
+        self.hours = hours
+        self.minutes = minutes
+        self.seconds = seconds
+
+    def get_time(self):
+        def make_double_num(num):
+            if num < 10:
+                return '0' + str(num)
+            return str(num)
+
+        seconds = make_double_num(self.seconds)
+        minutes = make_double_num(self.minutes)
+        hours = make_double_num(self.hours)
+        return f"{hours}:{minutes}:{seconds}"
+
+    def next_second(self):
+        self.seconds += 1
+        if self.seconds > Time.max_seconds:
+            self.seconds = 0
+            self.minutes += 1
+            if self.minutes > Time.max_minutes:
+                self.minutes = 0
+                self.hours += 1
+                if self.hours > Time.max_hours:
+                    self.hours = 0
+        return self.get_time()
+
+
+time = Time(9, 30, 59)
+print(time.next_second())
+
+time = Time(10, 59, 59)
+print(time.next_second())
+
+time = Time(23, 59, 59)
+print(time.next_second())
