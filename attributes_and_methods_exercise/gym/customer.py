5e4b79e75971532c7f1cbe57b9202bea5c0ab9fc
Asen Kamboshev <lion963@mail.bg>
2021-02-26 13:00:27 +0200

    attributes_and_methods 26.02

--- attributes_and_methods_exercise/gym/customer.py
+++ attributes_and_methods_exercise/gym/customer.py
@@ -1,16 +1,42 @@
+from attributes_and_methods_exercise.gym.equipment import Equipment
+from attributes_and_methods_exercise.gym.exercise_plan import ExercisePlan
+from attributes_and_methods_exercise.gym.gym import Gym
+from attributes_and_methods_exercise.gym.subscription import Subscription
+from attributes_and_methods_exercise.gym.trainer import Trainer
+
+
 class Customer:
-    __id = 0
+    id = 1
 
     def __init__(self, name, address, email):
         self.name = name
         self.address = address
         self.email = email
-        type(self).__id += 1
-        self.id = Customer.__id
+        self.id = Customer.id
+        Customer.id += 1
 
     @staticmethod
-    def get_next_id(self):
-        return self.id + 1
+    def get_next_id():
+        return Customer.id
 
     def __repr__(self):
         return f"Customer <{self.id}> {self.name}; Address: {self.address}; Email: {self.email}"
+
+
+customer = Customer("John", "Maple Street", "john.smith@gmail.com")
+equipment = Equipment("Treadmill")
+trainer = Trainer("Peter")
+subscription = Subscription("14.05.2020", 1, 1, 1)
+plan = ExercisePlan(1, 1, 20)
+
+gym = Gym()
+
+gym.add_customer(customer)
+gym.add_equipment(equipment)
+gym.add_trainer(trainer)
+gym.add_plan(plan)
+gym.add_subscription(subscription)
+
+print(Customer.get_next_id())
+
+print(gym.subscription_info(1))
