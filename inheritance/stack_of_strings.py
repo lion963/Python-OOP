35d6156d99cab9972dd2480e7f44439f2072c66d
Asen Kamboshev <lion963@mail.bg>
2021-03-04 11:05:46 +0200

    inheritance 04.03

--- inheritance/stack_of_strings.py
+++ inheritance/stack_of_strings.py
@@ -1,6 +1,6 @@
 class Stack():
     def __init__(self):
-        self.data=[]
+        self.data = []
 
     def push(self, item):
         self.data.insert(0, item)
@@ -9,46 +9,19 @@ class Stack():
         return self.data.pop(0)
 
     def peek(self):
-        return self.data.pop()
-
+        return self.data[-1]
 
     def is_empty(self):
-        return True if self.data else False
+        return True if not self.data else False
 
-class str(str, Stack):
-    def __init__(self, obj):
-        super().__init__()
-        self.obj = obj
-
-    def __repr__(self):
-        result="["
-        result+=f"{', '.join(self.obj.data)}"
-        result+="]"
-        return result
+    def __str__(self):
+        result = "["
+        result += f"{', '.join(self.data)}"
+        result += "]"
+        return f'{result}'
 
 
-# stack = Stack()
-# stack.push("apple")
-# stack.push("carrot")
-# print(str(stack))
-
-
-# test zero
-import unittest
-
-
-class StackTests(unittest.TestCase):
-    def test_zero(self):
-        stack = Stack()
-        stack.push("apple")
-        stack.push("carrot")
-        self.assertEqual(str(stack), '[carrot, apple]')
-        self.assertEqual(stack.pop(), 'carrot')
-        self.assertEqual(stack.peek(), 'apple')
-        stack.push("cucumber")
-        self.assertEqual(str(stack), '[cucumber, apple]')
-        self.assertEqual(stack.is_empty(), False)
-
-
-if __name__ == '__main__':
-    unittest.main()
+stack = Stack()
+stack.push("apple")
+stack.push("carrot")
+print(str(stack))
