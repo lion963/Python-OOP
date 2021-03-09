0483fe5fad8a4f6ee6ac2aec9fb06c682692e6c7
Asen Kamboshev <71500381+lion963@users.noreply.github.com>
2021-03-09 22:49:35 +0200

    Update salmon.py

--- inheritance_exercise/restaurant/food/salmon.py
+++ inheritance_exercise/restaurant/food/salmon.py
@@ -1,12 +1,10 @@
 from inheritance_exercise.restaurant.food.main_dish import MainDish
 
 class Salmon(MainDish):
-    __GRAMS = 22
+    GRAMS = 22
 
     def __init__(self, name, price):
-        super().__init__(name, price, Salmon.__GRAMS)
+        super().__init__(name, price, Salmon.GRAMS)
 
-    @property
-    def GRAMS(self):
-        return self.__GRAMS
+  
 
