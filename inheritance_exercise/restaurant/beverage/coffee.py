87f63b65727a9253b30ce6c557e3622820d99847
Asen Kamboshev <71500381+lion963@users.noreply.github.com>
2021-03-09 22:48:04 +0200

    Update coffee.py

--- inheritance_exercise/restaurant/beverage/coffee.py
+++ inheritance_exercise/restaurant/beverage/coffee.py
@@ -1,23 +1,15 @@
 from inheritance_exercise.restaurant.beverage.hot_beverege import HotBeverage
 
 class Coffee(HotBeverage):
-    __MILLILITERS = 50
-    __PRICE = 3.50
+    MILLILITERS = 50
+    PRICE = 3.50
 
     def __init__(self, name, caffeine):
-        super().__init__(name, Coffee.__PRICE, Coffee.__MILLILITERS)
+        super().__init__(name, Coffee.PRICE, Coffee.MILLILITERS)
         self.__caffeine = caffeine
 
     @property
     def caffeine(self):
         return self.__caffeine
-
-    @property
-    def MILLILITERS(self):
-        return self.__MILLILITERS
-
-    @property
-    def PRICE(self):
-        return self.__PRICE
 
 
