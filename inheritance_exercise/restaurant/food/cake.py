77b911c0b65e405da1e3ce96edcfcc35d4c9b466
Asen Kamboshev <71500381+lion963@users.noreply.github.com>
2021-03-09 22:50:30 +0200

    Update cake.py

--- inheritance_exercise/restaurant/food/cake.py
+++ inheritance_exercise/restaurant/food/cake.py
@@ -1,22 +1,11 @@
 from inheritance_exercise.restaurant.food.dessert import Dessert
 
 class Cake(Dessert):
-    __GRAMS = 250
-    __CALORIES = 1000
-    __PRICE = 5
+    GRAMS = 250
+    CALORIES = 1000
+    PRICE = 5
 
     def __init__(self, name):
-        super().__init__(name, Cake.__PRICE, Cake.__GRAMS, Cake.__CALORIES)
+        super().__init__(name, Cake.PRICE, Cake.GRAMS, Cake.CALORIES)
 
-    @property
-    def GRAMS(self):
-        return self.__GRAMS
-
-    @property
-    def CALORIES(self):
-        return self.__CALORIES
-
-    @property
-    def PRICE(self):
-        return self.__PRICE
 
