d06c201e3b2ab502184d76c837710051aa1e67b8
Asen Kamboshev <lion963@mail.bg>
2021-03-11 10:14:06 +0200

    solid 11.03

--- solid exercise/prisoner.py
+++ solid exercise/prisoner.py
@@ -5,20 +5,27 @@ class Person:
     def __init__(self, position):
         self.position = position
 
+
+class FreePerson(Person):
+    def __init__(self, position):
+        super().__init__(position)
+        self.is_free = True
+
     def walk_north(self, dist):
-        self.position[1] += dist
+        if self.is_free:
+            self.position[1] += dist
 
     def walk_east(self, dist):
-        self.position[0] += dist
+        if self.is_free:
+            self.position[0] += dist
 
 
-class Prisoner(Person):
+class Prisoner(FreePerson):
     PRISON_LOCATION = [3, 3]
 
     def __init__(self):
-        super(Prisoner, self).__init__(copy.copy(self.PRISON_LOCATION))
+        super().__init__(copy.copy(self.PRISON_LOCATION))
         self.is_free = False
-
 
 
 prisoner = Prisoner()
