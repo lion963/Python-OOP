ae318ab658065394615200b10292b529b58caa41
Asen Kamboshev <lion963@mail.bg>
2021-04-01 11:15:04 +0300

    exams_preparation 01.04

--- exam_preparation/exam_16_08_2020/project/software/express_software.py
+++ exam_preparation/exam_16_08_2020/project/software/express_software.py
@@ -2,7 +2,6 @@ from exam_preparation.exam_16_08_2020.project.software.software import Software
 
 
 class ExpressSoftware(Software):
-    type = "Express"
 
     def __init__(self, name, capacity_consumption, memory_consumption):
         super().__init__(name, "Express", int(capacity_consumption), int(memory_consumption * 2))
