18b43470ebe3279847bf18cc83e9dda69e97089c
Asen Kamboshev <lion963@mail.bg>
2021-04-01 11:12:14 +0300

    exams_preparation 01.04

--- exam_preparation/exam_16_08_2020/tests/test_hardware.py
+++ exam_preparation/exam_16_08_2020/tests/test_hardware.py
@@ -1,7 +1,9 @@
 from exam_preparation.exam_16_08_2020.project.hardware.hardware import Hardware
+from exam_preparation.exam_16_08_2020.project.software.software import Software
 from exam_preparation.exam_16_08_2020.project.software.light_software import LightSoftware
 from exam_preparation.exam_16_08_2020.project.software.express_software import ExpressSoftware
 import unittest
+
 
 
 class TestHardware(unittest.TestCase):
@@ -67,34 +69,35 @@ class TestHardware(unittest.TestCase):
         expected_result = [light_software, light_software2]
         self.assertEqual(result, expected_result)
 
-    # def test_get_light_software_components_count(self):
-    #     params = ['Linux', "Power", 200, 300]
-    #     hardware = Hardware(*params)
-    #     light_software = LightSoftware('SSD', 50, 60)
-    #     express_software = ExpressSoftware('HHD', 60, 70)
-    #     hardware.install(light_software)
-    #     hardware.install(express_software)
-    #     result = hardware.get_light_software_components_count()
-    #     expected_result = 1
-    #     self.assertEqual(result, expected_result)
-
-    # def test_get_express_software_components_count(self):
-    #     params = ['Linux', "Power", 200, 300]
-    #     hardware = Hardware(*params)
-    #     light_software = LightSoftware('SSD', 50, 60)
-    #     express_software = ExpressSoftware('HHD', 60, 70)
-    #     hardware.install(light_software)
-    #     hardware.install(express_software)
-    #     result = hardware.get_express_software_components_count()
-    #     expected_result = 1
-    #     self.assertEqual(result, expected_result)
-
-    # def test__repr__method(self):
-    #     params = ['Linux', "Power", 200, 300]
-    #     hardware = Hardware(*params)
-    #     result = hardware.__repr__()
-    #     expected_result = "Hardware Component - Linux\nExpress Software Components: 0\nLight Software Components: 0\nMemory Usage: 0 / 300\nCapacity Usage: 0 / 200\nType: Power\nSoftware Components: None"
-    #     self.assertEqual(result, expected_result)
+    # def setUp(self):
+    #     self.h = Hardware("Lenovo", "Power", 800, 32)
+    #     self.s = Software("Win", "Light", 10, 20)
+    #     self.e = Software("Win", "Express", 1000, 2000)
+    #
+    # def test_init(self):
+    #     self.assertEqual(self.h.name, "Lenovo")
+    #     self.assertEqual(self.h.type, "Power")
+    #     self.assertEqual(self.h.capacity, 800)
+    #     self.assertEqual(self.h.memory, 32)
+    #
+    # def test_empty_software_components(self):
+    #     self.assertEqual(self.h.software_components, [])
+    #
+    # def test_add_to_software_components(self):
+    #     self.h.install(self.s)
+    #     self.assertEqual(self.h.software_components, [self.s])
+    #
+    # def test(self):
+    #     self.h.install(self.s)
+    #     self.assertEqual(self.h.software_components, [self.s])
+    #     self.h.uninstall(self.s)
+    #     self.assertEqual(self.h.software_components, [])
+    #
+    # def test_cannot_add_due_to_high_requirements(self):
+    #     with self.assertRaises(Exception) as ex:
+    #         self.h.install(self.e)
+    #     self.assertEqual("Software cannot be installed", str(ex.exception))
+    #     self.assertEqual(self.h.software_components, [])
 
 
 if __name__ == '__main__':
